fd87185ca17d4b989d23fe94c2153654223c99c8
tkubrak <taras.kubrak@dataart.com>
2022-12-10 12:11:59 +0200

    passing data from parent to child component

---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {Post} from "../models/Post";
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent {
+  @Input() post: Post;
+
+  constructor() {
+    this.post = {
+      id: 0,
+      title: '',
+      body: '',
+      votes: 1
+    }
+  }
+}
